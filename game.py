7671bbac12a7733fb5e6eedf1d769c7b75e4b420
Erik Potter <epottercg@gmail.com>
2023-11-15 23:25:12 -0700

    Add dice, Game; require two rosters

--- game.py
+++ game.py
@@ -1,14 +1,28 @@
 import csv
 import logging
+import random
 import sys
 
 from enums import Handedness, PitcherDice, Positions, Traits
 
+
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
-
 # CONFIGURATION
-N_INNINGS = 9
+N_INNINGS    = 9
+N_BALLS_AB   = 4
+N_STRIKES_AB = 3
+
+
+def roll(kind: str) -> int:
+    count, n_sides = kind.split('d')
+    count   = int(count) if count else 1
+    n_sides = int(n_sides)
+    result = 0
+    for n in range(count):
+        result += random.randrange(1, n_sides+1)
+    logging.debug("Rolled %s -> %s", kind, result)
+    return result
 
 
 class Player:
@@ -65,9 +79,11 @@ class Team:
 
         logging.debug("Set lineup: %s", ', '.join([ str(n) for n in self.lineup ]))
 
-    def retire(self, player: Player):
+    def retire(self, player: Player) -> int:
+        i = self.lineup.index(player)
         self.lineup.remove(player)
         self._retired.add(player)
+        return i
 
     def is_available(self, player):
         return player not in self.retired
@@ -78,6 +94,12 @@ class Team:
             if p.number == n
         ][0]
 
+    def sub_player(self, entering: Player, leaving: Player):
+        assert entering in self.bench
+
+        i = self.retire(leaving)
+        self.lineup.insert(i, entering)
+
     def print_lineup(self):
         result = []
         for player in self.lineup:
@@ -85,42 +107,44 @@ class Team:
         print('\n'.join(result))
 
 
-def clean_row(n, row):
-    return {
-        'bt'    : int(row['BT']),
-        'hand'  : Handedness[row['Handedness']],
-        'name'  : row['Name'],
-        'number': n,
-        'obt'   : int(row['OBT']),
-        'pd'    : PitcherDice[row['PD']] if row['PD'] else None,
-        'pos'   : Positions[row['Position']],
-        'traits': [ Traits[t] for t in row['Traits'].split() ],
-    }
+class Game:
+    def __init__(self, teams=None):
+        self.teams = teams or []
 
-def load_roster(filename):
-    with open(roster_filename) as fh:
-        reader = csv.DictReader(fh)
-        return [ clean_row(n, row) for n, row in enumerate(reader) ]
+    def clean_row(self, n, row):
+        return {
+            'bt'    : int(row['BT']),
+            'hand'  : Handedness[row['Handedness']],
+            'name'  : row['Name'],
+            'number': n,
+            'obt'   : int(row['OBT']),
+            'pd'    : PitcherDice[row['PD']] if row['PD'] else None,
+            'pos'   : Positions[row['Position']],
+            'traits': [ Traits[t] for t in row['Traits'].split() ],
+        }
+
+    def get_team_from_roster(self, filename) -> Team:
+        with open(filename) as fh:
+            reader = csv.DictReader(fh)
+            roster = [ self.clean_row(n, row) for n, row in enumerate(reader) ]
+
+        players = [ Player(**row) for row in roster ]
+        team_name = filename.split('roster__')[-1].split('.')[0].replace('_', ' ').title()
+        logging.debug("Loaded team '%s' from roster file: %s", team_name, filename)
+        return Team(team_name, players=players)
 
 
 if __name__ == '__main__':
-    # load roster
-    roster_filename = sys.argv[1]
-    roster = load_roster(roster_filename)
+    roster_filename_a = sys.argv[1]
+    roster_filename_b = sys.argv[2]
 
-    # init team
-    team_name = roster_filename.split('roster__')[-1].split('.')[0].replace('_', ' ').title()
+    game = Game()
+    game.teams.append(game.get_team_from_roster(roster_filename_a))
+    game.teams.append(game.get_team_from_roster(roster_filename_b))
 
-    print()
-    print(team_name)
-    print("=" * len(team_name))
+    if roster_filename_a == roster_filename_b:
+        game.teams[1].name = "The Dopplegangers"
 
-    players = [ Player(**row) for row in roster ]
-    team = Team(team_name, players=players)
-
-    for player in team.players:
-        print()
-        print(player)
-
-    team.set_lineup()
-    team.print_lineup()
+    for team in game.teams:
+        team.set_lineup()
+        team.print_lineup()
--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import pytest
 
+from game import roll
+
 
 def test_retired_player_is_available(empty_team, player):
     empty_team.players.add(player)
@@ -9,3 +11,26 @@ def test_retired_player_is_available(empty_team, player):
 
 def test_bench_size(team):
     assert len(team.bench) == 40 - 9
+
+def test_sub_player(team):
+    entering = team.get_player_by_number(10)
+    leaving  = team.get_player_by_number(1)
+    team.sub_player(entering, leaving)
+    assert entering in team.lineup
+    assert leaving in team.retired
+    assert team.is_available(entering) is True
+    assert team.is_available(leaving) is False
+
+def test_roll():
+    for n in range(100):
+        assert 0 < roll('d4') < 5
+        assert 0 < roll('d8') < 9
+        assert 0 < roll('d10') < 11
+        assert 0 < roll('d12') < 13
+        assert 0 < roll('d20') < 21
+        assert 0 < roll('d100') < 101
+
+        assert 0 < roll('1d4') < 5
+        assert 1 < roll('2d4') < 9
+        assert 3 < roll('4d4') < 17
+        assert 1 < roll('2d10') < 21
